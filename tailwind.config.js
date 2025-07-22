bc6c734e06192fbe2fcc172e7bd58a8481f70860
Sabarni Guha <sabarni.guha2024@uem.edu.in>
2025-07-22 23:39:21 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
-  darkMode: "media", // or 'class'
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  darkMode: 'class', // Enables dark mode with class strategy
   theme: {
     extend: {},
   },
