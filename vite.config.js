9a824fadd1b6ca6b87109ba71c32ef1ec66713cd
Sabarni Guha <sabarni.guha2024@uem.edu.in>
2025-07-22 23:43:28 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    proxy: {
-      '/chat': 'http://localhost:5000',
-      '/ping': 'http://localhost:5000'
-    }
-  }
-});
+  },
+})
