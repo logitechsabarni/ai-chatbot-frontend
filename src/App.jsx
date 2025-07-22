f91675849543aa38549e84f4a6a36e5cb8c28b99
Sabarni Guha <sabarni.guha2024@uem.edu.in>
2025-07-22 20:48:19 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import ChatBox from "./components/ChatBox";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <ChatBox />
+    </div>
+  );
+};
+
+export default App;
